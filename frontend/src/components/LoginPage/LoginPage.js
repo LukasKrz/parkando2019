649441447d32233f62dc4e648df4c8aef378741e
Lukasz Krzyszkowiak <lukasz.krzyszkowiak@capgemini.com>
2019-06-01 15:04:55 +0200

    setState on response added

--- frontend/src/components/LoginPage/LoginPage.js
+++ frontend/src/components/LoginPage/LoginPage.js
@@ -1,139 +1,158 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
-import { withRouter } from "react-router-dom";
+import {withRouter} from "react-router-dom";
 
 import users from '../../mocks/users.js';
 import bigLogo from '../../images/parkando.png';
 
 class LoginPage extends Component {
-  state = {
-    userName: '',
-    userSurname: '',
-    card_id: '',
-    user_type: '',
-    message: '',
-    test: '',
-    
-      errors: {
-        userName: false,
-        userSurname: false,
-        card_id: false,
-        userInDB: false,
-      }
+    state = {
+        userName: '',
+        userSurname: '',
+        card_id: '',
+        user_type: '',
+        message: '',
+        test: '',
+
+        errors: {
+            userName: false,
+            userSurname: false,
+            card_id: false,
+            userInDB: false,
+        }
     }
-    
+
     messages = {
-      userNameIncorrect: 'Nazwa musi być dłuższa niż 3 znaki', // "i nie może zawierać spacji" - info: TEST
-      userSurnameIncorrect: 'Nazwa musi być dłuższa niż 3 znaki',
-      card_idIncorrect: 'Numer karty musi zawierać 3 cyfry',
-      noUserInDB: 'Błędny numer karty lub dane użytkownika',
+        userNameIncorrect: 'Nazwa musi być dłuższa niż 3 znaki', // "i nie może zawierać spacji" - info: TEST
+        userSurnameIncorrect: 'Nazwa musi być dłuższa niż 3 znaki',
+        card_idIncorrect: 'Numer karty musi zawierać 3 cyfry',
+        noUserInDB: 'Błędny numer karty lub dane użytkownika',
     }
-    
+
     handleChange = e => {
-      const name = e.target.name;
-      const type = e.target.type;
-    
-      if(type === "text") {
-        const value = e.target.value;
-        this.setState({
-          [name]: value
-        })
-      }
-    }
-    
-    handleSubmit = e => {
-      e.preventDefault();
-      const validation = this.formValidation();
-    
-      if(validation.correct) {
-        fetch('/api/studenci/walidacja',{
-          method: 'POST',
-          headers: {
-          Accept: 'application/json',
-                  'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            card_id: this.state.card_id,
-            name: this.state.userName,
-            surname: this.state.userSurname
-          })
-          }).then(response => {
-            response.text().then(function(data) {
-              const objectResponse = JSON.parse(data);
-              this.setState({
-                userName: objectResponse.name,
-                userSurname: objectResponse.surname,
-                card_id: objectResponse.card_id,
-                user_type: objectResponse.user_type,
-                test: objectResponse.card_id
-              });
+        const name = e.target.name;
+        const type = e.target.type;
+
+        if (type === "text") {
+            const value = e.target.value;
+            this.setState({
+                [name]: value
             })
-            // console.log('RES: ', response.text());
-            // this.setState({test: response.card_id});
-          })
-          .catch(error =>{
-            console.log(error);
-          })  
-
-
-        this.props.logUser(this.state.card_id, this.state.userName, this.state.userSurname); // dzienny zaoczny?
-
-        //   // this.props.logUser(this.state.card_id, this.state.userName, this.state.userSurname);
-          
-        //   if(park_place_id === null) {
-        //     this.props.history.push(`/welcome/${this.state.card_id}`);
-        //   } else {
-        //     this.props.history.push(`/final-confirmation/${this.state.card_id}/${park_place_id}`)
-        //   }
-
-        //   this.setState({
-        //     errors: {
-        //       userInDB: false,
-        //     }
-        //   })          
-        // } else {
-        //   this.setState({
-        //     errors: {
-        //       userInDB: true,
-        //     }
-        //   })
-        // }
-      } else {
-        this.setState({
-          errors: {
-            userName: !validation.userName,
-            userSurname: !validation.userSurname,
-            card_id: !validation.card_id,
-          }
-        })
-      }
+        }
     }
-    
+
+    handleSubmit = e => {
+        e.preventDefault();
+        const validation = this.formValidation();
+
+        if (validation.correct) {
+
+        // let test = null;
+            fetch('/api/studenci/walidacja', {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    card_id: this.state.card_id,
+                    name: this.state.userName,
+                    surname: this.state.userSurname
+                })
+            }).then(response => response.json())
+                .then(data => {
+                    this.setState({
+                        userName: data.name,
+                        userSurname: data.surname ,
+                        card_id: data.card_id ,
+                        user_type: data.user_type,
+                        test: data
+                    })})
+                    // response.text().then(function (data) {
+                    //     const objectResponse = JSON.parse(data);
+                    //     test = objectResponse;
+                    //     // const num = objectResponse.card_id;
+                    //     // console.log('card', objectResponse.card_id);
+                    //     // this.props.logUser(objectResponse.card_id, objectResponse.userName, objectResponse.userSurname);
+                    //     // if (objectResponse) {
+                    //     //     this.setState({
+                    //     //     //     userName: objectResponse ? objectResponse.name : '',
+                    //     //     //     userSurname: objectResponse ? objectResponse.surname : '',
+                    //     //     //     card_id: objectResponse ? objectResponse.card_id : '',
+                    //     //     //     user_type: objectResponse ? objectResponse.user_type : '',
+                    //     //         test: objectResponse
+                    //     //     });
+                    //     // }
+                    //
+                    // })
+                    // console.log('RES: ', response.text());
+                    // this.setState({test: response.card_id});
+                    // })
+                        .catch(error => {
+                            console.log(error);
+                        })
+                // }
+
+ // this.setState({test: test});
+
+            // this.props.logUser(this.state.card_id, this.state.userName, this.state.userSurname); // dzienny zaoczny?
+
+            //   // this.props.logUser(this.state.card_id, this.state.userName, this.state.userSurname);
+
+            //   if(park_place_id === null) {
+            //     this.props.history.push(`/welcome/${this.state.card_id}`);
+            //   } else {
+            //     this.props.history.push(`/final-confirmation/${this.state.card_id}/${park_place_id}`)
+            //   }
+
+            //   this.setState({
+            //     errors: {
+            //       userInDB: false,
+            //     }
+            //   })
+            // } else {
+            //   this.setState({
+            //     errors: {
+            //       userInDB: true,
+            //     }
+            //   })
+            // }
+        } else {
+            this.setState({
+                errors: {
+                    userName: !validation.userName,
+                    userSurname: !validation.userSurname,
+                    card_id: !validation.card_id,
+                }
+            })
+        }
+    }
+
     formValidation = () => {
-      let userName = false;
-      let userSurname = false;
-      let card_id = false;
-      let correct = false;
-    
-      if(this.state.userName.length > 3) { // && this.state.userName.indexOf(' ') === -1 - info TEST
-        userName = true;
-      }
-      if(this.state.userSurname.length > 3) {
-        userSurname = true;
-      }
-      if(this.state.card_id.length === 3 && !isNaN(this.state.card_id)) {
-        card_id = true;
-      }
-      if(userName && userSurname && card_id) {
-        correct = true;
-      }
-    
-      return ({
-        correct,
-        userName,
-        userSurname,
-        card_id,
-      })
+        let userName = false;
+        let userSurname = false;
+        let card_id = false;
+        let correct = false;
+
+        if (this.state.userName.length > 3) { // && this.state.userName.indexOf(' ') === -1 - info TEST
+            userName = true;
+        }
+        if (this.state.userSurname.length > 3) {
+            userSurname = true;
+        }
+        if (this.state.card_id.length === 3 && !isNaN(this.state.card_id)) {
+            card_id = true;
+        }
+        if (userName && userSurname && card_id) {
+            correct = true;
+        }
+
+        return ({
+            correct,
+            userName,
+            userSurname,
+            card_id,
+        })
     }
 
     // TODO findUser, userParkingSpaceValidation, setUserType probably remove or modified when backend is in
@@ -180,61 +199,80 @@ class LoginPage extends Component {
     //   user.map(el => type = el.user_type)
     //   return type;
     // }
-    
+
     componentDidUpdate() {
-      if(this.state.message !== '') {
-        setTimeout(() => this.setState({message: ''}), 2000);
-      }
+        if (this.state.message !== '') {
+            setTimeout(() => this.setState({message: ''}), 2000);
+        }
     }
 
     render() {
-      console.log('TEST: ', this.state.test);
-      
-      return (
-        <div className="login-container">
-          <div className="login-container__logo">
-            <img src={bigLogo} alt="parkando-logo"/>
-          </div>
-          <form className="login-container__form form" onSubmit={this.handleSubmit} noValidate>
-            {this.state.errors.userInDB && <span className="simple-label__error error--top">{this.messages.noUserInDB}</span>}
-            <label htmlFor="number" className="form__simple-label simple-label">Podaj nr karty: 
-              <input
-                type="text"
-                id="number"
-                name="card_id"
-                value={this.state.card_id}
-                onChange={this.handleChange}
-                className="simple-label__input"
-              />
-              {this.state.errors.card_id && <span className="simple-label__error">{this.messages.card_idIncorrect}</span>}
-            </label>
-            <label htmlFor="name" className="form__simple-label simple-label">Imię: 
-              <input
-                type="text"
-                id="name"
-                name="userName"
-                value={this.state.userName}
-                onChange={this.handleChange}
-                className="simple-label__input"
-              />
-              {this.state.errors.userName && <span className="simple-label__error">{this.messages.userNameIncorrect}</span>}
-            </label>
-            <label htmlFor="surname" className="form__simple-label simple-label">Nazwisko: 
-              <input
-                type="text"
-                id="surname"
-                name="userSurname"
-                value={this.state.userSurname}
-                onChange={this.handleChange}
-                className="simple-label__input"
-              />
-              {this.state.errors.userSurname && <span className="simple-label__error">{this.messages.userSurnameIncorrect}</span>}
-            </label>
-            <button className="form__login-btn">Zaloguj</button>
-          </form>
-          {this.state.message && <h3>{this.state.message}</h3>}
-        </div>
-  )}
+        console.log('TEST: ', this.state.test, this.state.userName);
+
+        return (
+            < div
+        className = "login-container" >
+            < div
+        className = "login-container__logo" >
+            < img
+        src = {bigLogo}
+        alt = "parkando-logo" / >
+            < /div>
+            < form
+        className = "login-container__form form"
+        onSubmit = {this.handleSubmit}
+        noValidate >
+        {
+            this.state.errors.userInDB && < span className = "simple-label__error error--top" > {this.messages.noUserInDB} < /span>}
+            < label htmlFor = "number" className = "form__simple-label simple-label" > Podaj nr karty
+    :
+    <
+        input
+        type = "text"
+        id = "number"
+        name = "card_id"
+        value = {this.state.card_id}
+        onChange = {this.handleChange}
+        className = "simple-label__input"
+            / >
+            {
+                this.state.errors.card_id && < span className = "simple-label__error" > {this.messages.card_idIncorrect} < /span>}
+                < /label>
+                < label htmlFor = "name" className = "form__simple-label simple-label" > Imię
+    :
+    <
+        input
+        type = "text"
+        id = "name"
+        name = "userName"
+        value = {this.state.userName}
+        onChange = {this.handleChange}
+        className = "simple-label__input"
+            / >
+            {
+                this.state.errors.userName && < span className = "simple-label__error" > {this.messages.userNameIncorrect} < /span>}
+                < /label>
+                < label htmlFor = "surname" className = "form__simple-label simple-label" > Nazwisko
+    :
+    <
+        input
+        type = "text"
+        id = "surname"
+        name = "userSurname"
+        value = {this.state.userSurname}
+        onChange = {this.handleChange}
+        className = "simple-label__input"
+            / >
+            {
+                this.state.errors.userSurname && < span className = "simple-label__error" > {this.messages.userSurnameIncorrect} < /span>}
+                < /label>
+                < button className = "form__login-btn" > Zaloguj < /button>
+                < /form>
+        {
+            this.state.message && < h3 > {this.state.message} < /h3>}
+            < /div>
+        )
+    }
 }
 
 export default withRouter(LoginPage);
