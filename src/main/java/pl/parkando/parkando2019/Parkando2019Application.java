e39730b5d4cfa59e3e1443b0151080f45a96b7f3
Lukasz Krzyszkowiak <lukasz.krzyszkowiak@capgemini.com>
2019-06-01 08:37:23 +0200

    basis of App - working back- and frontend side of the application, comunication between back- and frontend

---
+++ src/main/java/pl/parkando/parkando2019/Parkando2019Application.java
@@ -0,0 +1,13 @@
+package pl.parkando.parkando2019;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Parkando2019Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Parkando2019Application.class, args);
+	}
+
+}
