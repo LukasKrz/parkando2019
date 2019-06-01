c4b0770d2ccf973a00069ba8b1bedca547d2bf38
MartaKMB <muszkamarta@interia.pl>
2019-06-01 10:14:42 +0200

    MARTA-FRONT-1 clean up code from useless folders

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -63,6 +63,19 @@ class App extends Component {
       .then(json => this.setState({ test: json.results }));
   }
 
+  // componentDidMount() {
+  //     setInterval(this.hello, 250);
+  // }
+
+  // hello = () => {
+  // fetch('/api/hello')
+  //  .then(response => response.text())
+  //  .then(message => {
+  //     this.setState({message: message});
+  //   });
+  // };
+
+
   render() {
     this.state.test.map(e => console.log(e));      
     return (
